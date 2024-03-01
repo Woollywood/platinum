018fb91f5e3728d91b22b92455c65707d6762c27
woollywood <Gerbert.Wells969@yandex.ru>
2024-03-01 04:04:13 +0300

    anchor links

--- src/js/app.js
+++ src/js/app.js
@@ -211,7 +211,7 @@ import * as flsScroll from './custom/scroll.js';
 
 // Плавная навигация по странице
 // Документация: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
-// flsScroll.pageNavigation();
+flsScroll.pageNavigation();
 // Функционал добавления классов к хедеру во время прокрутки
 // Документация: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
 // flsScroll.headerScroll();
